8fea7d33fd2a1b5da4c661af4c3709dff030ceee
kz1985 <khosruzzaman.ny@gmail.com>
2023-10-26 13:29:12 -0400

    Test case description added

--- src/test/java/com/Tests/DeshBoardHomeTest.java
+++ src/test/java/com/Tests/DeshBoardHomeTest.java
@@ -94,6 +94,9 @@ public class DeshBoardHomeTest extends TestBase {
 	*/
 
 	@AfterMethod(groups = "Initializations", description = "Close all browsers.")
+	 @Severity(SeverityLevel.NORMAL)
+    @Description("To closed the browsers")
+   
 	public void tearDown() {
 	 
 		driver.close();
